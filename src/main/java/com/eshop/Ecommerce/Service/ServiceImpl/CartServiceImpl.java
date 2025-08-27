50186c9ff0618611cf7e3304757e9097e2f08dee
Aryanyadav99 <aryankietstudent@gmail.com>
2025-08-27 21:04:49 +0530

    fix(cart): fix the quantity issue

--- src/main/java/com/eshop/Ecommerce/Service/ServiceImpl/CartServiceImpl.java
+++ src/main/java/com/eshop/Ecommerce/Service/ServiceImpl/CartServiceImpl.java
@@ -95,16 +95,23 @@ public class CartServiceImpl implements CartService {
         if(carts.isEmpty()){
             throw new APIException("No carts found");
         }
-        List<CartDTO> cartDTOs= carts.stream().
-                map((cart) ->{
-                    CartDTO cartDTO=modelMapper.map(cart, CartDTO.class);
-                    List<ProductDTO> products=cart.getCartItems().stream()
-                            .map(p->modelMapper.map(p.getProduct(), ProductDTO.class))
-                            .toList();
-                    cartDTO.setProducts(products);
-                    return cartDTO;
-                }).toList();
-        return  cartDTOs;
+        List<CartDTO> cartDTOs = carts.stream().map(cart -> {
+            CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+            List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+                ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+                productDTO.setQuantity(cartItem.getQuantity()); // Set the quantity from CartItem
+                return productDTO;
+            }).collect(Collectors.toList());
+
+
+            cartDTO.setProducts(products);
+
+            return cartDTO;
+
+        }).collect(Collectors.toList());
+
+        return cartDTOs;
     }
 
     @Override
