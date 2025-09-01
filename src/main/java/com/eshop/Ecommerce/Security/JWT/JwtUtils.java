e014a85e2e2e2c8c0a3bde74fa773f8e6f2c40cd
Aryanyadav99 <aryankietstudent@gmail.com>
2025-09-02 00:55:32 +0530

    feat(docs): add dependecy for swagger and downgrade spring version for swagger

--- src/main/java/com/eshop/Ecommerce/Security/JWT/JwtUtils.java
+++ src/main/java/com/eshop/Ecommerce/Security/JWT/JwtUtils.java
@@ -40,13 +40,13 @@ public class JwtUtils {
         }
     }
 
-//    public String getJwtFromHeader(HttpServletRequest request) {
-//        String bearerToken = request.getHeader("Authorization");
-//        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-//            return bearerToken.substring(7);
-//        }
-//        return null;
-//    }
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 
     //cookie generation
     public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
